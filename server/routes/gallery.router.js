97037eddaaedfd9a8cdbf1dd8daf719cfda863d7
Scott Schulte <scott.r.schulte@gmail.com>
2024-05-24 15:58:39 -0500

    Create a put route to change number of likes

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  const photoID = req.params.id;
+  const sqlText = `UPDATE gallery SET likes=(likes+1) WHERE id=$1;`;
+  pool
+    .query(sqlText, [photoID])
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(`error resetting all items:`, error);
+    });
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = 'SELECT * FROM gallery ORDER BY title;';
+  pool
+    .query(sqlText)
+    .then((result) => {
+      console.log(`Got gallery from the server:`, result.rows);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
